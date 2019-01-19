e7c7a39b9e66b29159d1be4e7bd7e3a934b01aac
ckdgus0204 <43057466+ckdgus0204@users.noreply.github.com>
2019-01-19 21:14:13 +0900

    Create 2446.java

---
+++ changhyun/2446.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<i;j++)
+        System.out.print(" ");
+      for(int j=0;j<2*(n-i)+1;j++)
+        System.out.print("*");
+
+      System.out.println();
+    }
+    for(int i=n-1;i>0;i--)
+    {
+      for(int j=1;j<i;j++)
+        System.out.print(" ");
+      for(int j=0;j<2*(n-i)+1;j++)
+        System.out.print("*");
+
+      System.out.println();
+    }
+  }
+}
