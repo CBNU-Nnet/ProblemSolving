9d78c9ed03174611ce9c4b9cffe7ffbc6ff096a5
ckdgus0204 <43057466+ckdgus0204@users.noreply.github.com>
2019-01-19 21:11:02 +0900

    Create 2739.java

---
+++ changhyun/2739.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		int A= sc.nextInt();
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(A+" * "+i+" = "+(A*i));
+		}
+
+		sc.close(); 
+	}
+}
